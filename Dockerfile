d0047897521f94abeb8dfa6dbc43be76c7106512
Daniel Oram <danieloramnz@gmail.com>
2021-01-26 23:01:28 +0000

    Check database is operating before starting web container

--- Dockerfile
+++ Dockerfile
@@ -16,5 +16,9 @@ RUN pip install --upgrade pip
 COPY ./requirements.txt .
 RUN pip install -r requirements.txt
 
+COPY ./entrypoint.sh .
+
 # copy project
 COPY . .
+
+ENTRYPOINT ["/usr/src/app/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if ["$DATABASE" = "postgres"]
+then
+    echo "Waiting for database..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+        sleep 0.1
+    done
+
+    echo "Database has started!"
+fi
+
+python manage.py flush --no-input
+python manage.py migrate
+
+exec "$@"
