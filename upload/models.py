2fbc0b61dbc2679134e08b2e384be0ee89ad6a65
Daniel Oram <danieloramnz@gmail.com>
2021-01-29 14:38:27 +0000

    add signal on post_delete of model to delete local file from path

--- upload/models.py
+++ upload/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from app.storage_backends import TestS3MediaStorage
 import os
 
+from django.dispatch import receiver
+
 # Create your models here.
+
 
 def upload_to_session_key_dir(instance, filename):
     return os.path.join(instance.session_key, filename)
@@ -11,3 +14,17 @@ class TestS3Upload(models.Model):
     session_key = models.CharField(max_length=50, null=False, blank=False)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     file = models.FileField(upload_to=upload_to_session_key_dir)
+
+
+# delete TestS3Upload file from local media 
+def _delete_file(path):
+   """ Deletes file from filesystem. """
+   print('deleting {} from filesystem')
+   if os.path.isfile(path):
+       os.remove(path)
+
+@receiver(models.signals.post_delete, sender=TestS3Upload)
+def delete_file(sender, instance, *args, **kwargs):
+    """ Deletes file on `post_delete` """
+    if instance.file:
+        _delete_file(instance.file.path)
